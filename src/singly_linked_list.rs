35bc584b43cf2bc652b6ed687b925e876e1d71c5
Aravindh Sridharan <mail@aravindh.io>
2018-10-21 16:51:11 +0200

    Added element_at and element_at_mut

--- src/singly_linked_list.rs
+++ src/singly_linked_list.rs
@@ -29,38 +29,63 @@ impl<T: Debug> SinglyLinkedList<T> {
         length
     }
 
-    pub fn add_at(&mut self, t: T, index: i32) -> &mut Self {
-        if index > self.length() {
-            return self;
-        } else {
-            let mut counter = index;
-            let mut current_node = &mut self.head;
-            while counter != 0 {
-                if current_node.is_some() {
-                    match current_node {
-                        Some(node) => {
-                            current_node = &mut node.next;
-                            counter = counter - 1;
-                        }
-                        None => {}
-                    }
-                }
-            }
+    pub fn element_at(&self, index: i32) -> Option<&T> {
+        let mut current_node = &self.head;
+        let mut counter = index;
+        while counter != 0 && current_node.is_some() {
+            let current_node_value = current_node.borrow_unwrap();
+            current_node = &current_node_value.next;
+            counter = counter - 1;
+        }
+        match current_node {
+            Some(node) => Some(&node.value),
+            None => None,
+        }
+    }
 
+    pub fn element_at_mut(&mut self, index: i32) -> Option<&mut T> {
+        let mut current_node = &mut self.head;
+        let mut counter = index;
+        while counter != 0 && current_node.is_some() {
+            let current_node_value = current_node.borrow_unwrap_mut();
+            current_node = &mut current_node_value.next;
+            counter = counter - 1;
+        }
+        match current_node {
+            Some(node) => Some(&mut node.value),
+            None => None,
+        }
+    }
+    /**
+     * if length of list < index, return error
+     * else
+     * Get node at index n-1
+     */
+    pub fn add_at(&mut self, t: T, index: i32) -> &mut Self {
+        let mut counter = index;
+        let mut current_node = &mut self.head;
+        while counter != 0 && current_node.is_some() {
             match current_node {
                 Some(node) => {
-                    let current_next = node.next.take();
-                    let new_node = Node {
-                        value: t,
-                        next: current_next,
-                    };
-                    node.next = Some(Box::new(new_node));
+                    current_node = &mut node.next;
+                    counter = counter - 1;
                 }
                 None => {}
-            };
-
-            self
+            }
         }
+        match current_node {
+            Some(node) => {
+                let current_next = node.next.take();
+                let new_node = Node {
+                    value: t,
+                    next: current_next,
+                };
+                node.next = Some(Box::new(new_node));
+            }
+            None => {}
+        };
+
+        self
     }
 
     pub fn add(&mut self, t: T) -> &mut Self {
@@ -106,5 +131,24 @@ impl<T: ToString> ToString for SinglyLinkedList<T> {
             }
         }
         vec.into_iter().map(|i| i.to_string()).collect::<String>()
+    }
+}
+
+trait OptionReferenceBorrow<T> {
+    fn borrow_unwrap(&self) -> &T;
+    fn borrow_unwrap_mut(&mut self) -> &mut T;
+}
+impl<T> OptionReferenceBorrow<T> for Option<T> {
+    fn borrow_unwrap(&self) -> &T {
+        match self {
+            Some(v) => v,
+            None => panic!("Unwrapping none"),
+        }
+    }
+    fn borrow_unwrap_mut(&mut self) -> &mut T {
+        match self {
+            Some(v) => v,
+            None => panic!("Unwrapping none"),
+        }
     }
 }
--- test/list/singly_linked_list_tests.rs
+++ test/list/singly_linked_list_tests.rs
@@ -20,7 +20,35 @@ fn add_at_works() {
     sll.add(2);
     sll.add(3);
     sll.add(5);
-    sll.add_at(4, 2);
+    sll.add_at(4, 0);
     println!("{:?}", sll);
     assert_eq!(sll.length(), 5);
+}
+
+#[test]
+fn get_element_at_works() {
+    let mut sll: SinglyLinkedList<i32> = SinglyLinkedList::new();
+    assert_eq!(sll.element_at(0), None);
+    sll.add(10);
+    sll.add(21);
+    sll.add(33);
+    sll.add(50);
+    assert_eq!(sll.element_at(0), Some(&10));
+    assert_eq!(sll.element_at(1), Some(&21));
+    assert_eq!(sll.element_at(2), Some(&33));
+    assert_eq!(sll.element_at(3), Some(&50));
+}
+
+#[test]
+fn get_element_at_mut_works() {
+    let mut sll: SinglyLinkedList<i32> = SinglyLinkedList::new();
+    assert_eq!(sll.element_at_mut(0), None);
+    sll.add(10);
+    sll.add(21);
+    sll.add(33);
+    sll.add(50);
+    assert_eq!(sll.element_at_mut(0), Some(&mut 10));
+    assert_eq!(sll.element_at_mut(1), Some(&mut 21));
+    assert_eq!(sll.element_at_mut(2), Some(&mut 33));
+    assert_eq!(sll.element_at_mut(3), Some(&mut 50));
 }
